7a3d48ac9fd841009c31b31728c6450292aaf985
jdiguetdickson <72795044+jdiguetdickson@users.noreply.github.com>
2021-12-22 14:34:12 +0100

    Add files via upload

---
+++ GCEException.java
@@ -0,0 +1,13 @@
+package com.thermador.formation.exception;
+
+public class GCEException extends Exception {
+
+    private static final long serialVersionUID = -5724073680165630462L;
+
+    public GCEException(final Throwable cause) {
+        super(cause);
+    }
+
+
+
+}
